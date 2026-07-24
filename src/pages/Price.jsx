278248e9cbbe2c7c6e402093d1f6a6091ab84564
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-24 15:22:16 -0400

    use URL parameter in Price page

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,3 +1,9 @@
-export default function Price() {
-  return <h1>This is the Price Component</h1>;
+import { useParams } from "react-router";
+
+export default function Price(props) {
+  // Grab the currency symbol from the URL parameter
+  const params = useParams();
+  const symbol = params.symbol;
+
+  return <h1>This is the Price Component for {symbol}</h1>;
 }
