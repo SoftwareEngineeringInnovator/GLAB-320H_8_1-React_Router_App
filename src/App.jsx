e20c2b64ff9b6c9fbdc3dc8d1575780f8f1615dc
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-24 14:01:25 -0400

    add/import Nav from the Nav.jsx file so each page will have a navigation link

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import { Route, Routes } from "react-router";
 import Main from "./pages/Main.jsx";
 import Currencies from "./pages/Currencies.jsx";
 import Price from "./pages/Price.jsx";
+import Nav from "./components/Nav.jsx";
 
 export default function App() {
   return (
     <div className="App">
+
+       {/* The navigation appears on every page. */}
+      <Nav />
+      
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/currencies" element={<Currencies />} />
