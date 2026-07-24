ad936ed04a6d0a0df83dd9d73d3a1aa9f347fb0a
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-24 16:28:59 -0400

    add CoinAPI key and request URL to Price page

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,9 +1,15 @@
 import { useParams } from "react-router";
 
 export default function Price(props) {
+  // Read the CoinAPI key
+  const apiKey = import.meta.env.VITE_COIN_API_KEY;
+
   // Grab the currency symbol from the URL parameter
   const params = useParams();
   const symbol = params.symbol;
+
+  // Build CoinAPI request URL using selected currency symbol
+  const url = `https://rest.coinapi.io/v1/exchangerate/${symbol}/?apikey=${apiKey}`;
 
   return <h1>This is the Price Component for {symbol}</h1>;
 }
